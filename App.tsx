bb9a7d996059aed3cb8fbbeac776603f9a660971
jcobarreto <julio.barreto@gmail.com>
2021-04-22 11:46:12 -0300

    Environments and plants navigation implemented

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AppLoading from "expo-app-loading";
 
-import { Welcome } from "./src/pages/Welcome";
+import Routes from "./src/routes";
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost";
 
 export default function App() {
@@ -14,5 +14,5 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <Welcome />;
+  return <Routes />;
 }
